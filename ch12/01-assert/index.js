562f3ff45d15ab48a2e46fd171832736d221684e
konosumi <36559308+konosumi@users.noreply.github.com>
2020-01-20 13:45:12 +0900

    Update index.js

--- ch12/01-assert/index.js
+++ ch12/01-assert/index.js
@@ -1,20 +1,26 @@
 
 const assert = require('assert').strict;
 
-assert.deepEqual([[1, 2, 3]], [[1, 2, 3]]);
-
+assertCheck([1, 2, 3]);
 // 数値の3と文字列の3が異なるため、エラーを出力して失敗する
-assert.deepEqual([[1, 2, 3]], [[1, 2, '3']]);
+assertCheck([1, 2, '3']);
+
+function assertCheck(c) {
+    assert.deepEqual(c, [1, 2, 3]);
+}
+
 /*
+assert.js:89
+  throw new AssertionError(obj);
+  ^
+
 AssertionError [ERR_ASSERTION]: Expected values to be strictly deep-equal:
 + actual - expected
 
   [
-    [
-      1,
-      2,
-+     3
--     '3'
-    ]
+    1,
+    2,
++   '3'
+-   3
   ]
 */
